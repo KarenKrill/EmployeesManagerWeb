822a412fb813a798bf0ec23b1a9565d1cf77c2bd
Art <Art@DESKTOP-666DEVI>
2021-09-29 16:02:33 +0500

    Добавьте файлы проекта.

---
+++ EmployeesManagerWeb/Controllers/EmployeesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EmployeesManagerWeb.Models;
+
+namespace EmployeesManagerWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private static EmployeesManager employeesManager = new EmployeesManager();
+        private static Progress<uint> addEmployeeProgress = null;
+        private static uint addEmployeeProgressValue = 0;
+        private void AddEmployeeProgress_ProgressChanged(object sender, uint e) { addEmployeeProgressValue = e; }
+        public EmployeesController()
+        {
+            if (addEmployeeProgress == null)
+            {
+                addEmployeeProgress = new Progress<uint>();
+                addEmployeeProgress.ProgressChanged += AddEmployeeProgress_ProgressChanged;
+            }
+        }
+        [HttpGet]
+        public Employee[] GetAll()
+        {
+            Employee[] employees = employeesManager.GetList();
+            return employees;
+        }
+        [HttpPost]
+        public async Task<bool> Post(Employee employee, System.Threading.CancellationToken cancelToken)
+        {
+            addEmployeeProgressValue = 0;
+            bool res = await employeesManager.AddAsync(employee, addEmployeeProgress, cancelToken);
+            return res;
+        }
+        [HttpGet("EmployeeAddingProgress")]
+        public uint EmployeeAddingProgress(){ return addEmployeeProgressValue; }
+        [HttpDelete("{id:int}")]
+        public bool Delete(uint id){ return employeesManager.Remove(id); }
+        [HttpGet("search/{id:int}")]
+        public Employee Search(uint id){ return employeesManager.Search(id); }
+    }
+}
---
+++ EmployeesManagerWeb/Models/EmployeesManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeesManagerWeb.Models
+{
+    internal class UidGenerator
+    {
+        private uint ID;
+        public UidGenerator(uint StartWith = 0){ ID = StartWith; }
+        public uint GetNextUID(){ return ID++; }
+    }
+    public class Employee
+    {
+        public uint ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public uint Salary { get; set; }
+    }
+    /// <summary>
+    /// Provides work with employee data
+    /// </summary>
+    public class EmployeesManager
+    {
+        private Dictionary<uint, Employee> employees = new Dictionary<uint, Employee>();
+        private UidGenerator uidGenerator = new UidGenerator((uint)new Random().Next());
+        public Employee[] GetList() { return employees.Values.ToArray(); }
+        public bool Add(Employee employee, IProgress<uint> progress = null, System.Threading.CancellationToken cancelToken = default)
+        {
+            lock (employees)//thread safety
+            {
+                uint n = 80;
+                for (uint i = 0; i < n; i++)
+                {
+                    progress?.Report(100 * i / n);
+                    if(cancelToken.IsCancellationRequested) return false;
+                    System.Threading.Thread.Sleep(20);
+                }
+                employee.ID = uidGenerator.GetNextUID();
+                employees.Add(employee.ID, employee);
+                progress?.Report(100);
+            }
+            return true;
+        }
+        public async Task<bool> AddAsync(Employee employee, Progress<uint> progress = null, System.Threading.CancellationToken cancellationToken = default)
+        {
+            bool result = false;
+            result = await Task.Run(() => Add(employee, progress, cancellationToken));
+            return result;
+        }
+        public bool Remove(uint ID){ return employees.Remove(ID); }
+        public Employee Search(uint ID)
+        {
+            Employee employee = null;
+            employees.TryGetValue(ID, out employee);
+            return employee;
+        }
+    }
+}
